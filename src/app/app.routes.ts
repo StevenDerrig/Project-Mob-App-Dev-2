436e5e1ed187e3907139abbcf2ce19a0290fa465
Steebo <G00424644@atu.ie>
2025-04-27 17:35:52 +0100

    Aded basic home page set up. Added ionic features, to be used through out the app.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './component/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home-page',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home-page',
+        component: HomePageComponent
+    },
+    {
+        path: 'logging-page',
+        loadComponent: () => import('./component/logging-page/logging-page.component').then(m => m.LoggingPageComponent)
+    },
+    {
+        path: 'daily-sum-page',
+        loadComponent: () => import('./component/daily-sum-page/daily-sum-page.component').then(m => m.DailySumPageComponent)
+    },
+    {
+        path: 'scan-prod-page',
+        loadComponent: () => import('./component/scan-prod-page/scan-prod-page.component').then(m => m.ScanProdPageComponent)
+    },
+    {
+        path: 'settings-page',
+        loadComponent: () => import('./component/settings-page/settings-page.component').then(m => m.SettingsPageComponent)
+    },
+    {
+        path: '**',
+        redirectTo: 'home-page'
+    }
+];
--- src/app/component/home-page/home-page.component.ts
+++ src/app/component/home-page/home-page.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [IonicModule],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  styleUrl: './home-page.component.css',
+  standalone: true,
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit{
+  constructor(private router: Router) { }
 
+  ngOnInit(): void {
+    // Initialization logic can go here
+  }
+
+  // Navigation methods
+  navigateToLogging(): void {
+    this.router.navigate(['/logging-page']);
+  }
+
+  navigateToDaily(): void {
+    this.router.navigate(['/daily-sum-page']);
+  }
+
+  navigateToScan(): void {
+    this.router.navigate(['/scan-prod-page']);
+  }
+
+  navigateToSettings(): void {
+    this.router.navigate(['/settings-page']);
+  }
 }

bf1a29f755dd0b071856c2e74773705e7b33468a
Steebo <G00424644@atu.ie>
2025-04-27 19:53:27 +0100

    Made changes to make the homepage display be as seen in the prototype.

--- src/app/component/home-page/home-page.component.ts
+++ src/app/component/home-page/home-page.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
   standalone: true,
 })
 export class HomePageComponent implements OnInit{
+  // Add date property
+  currentDate: Date = new Date();
+  
+  // User info
+  userName: string = '<Name>'; // Placeholder, would come from a service
+  
+  // Nutrient values - these would be updated from a service
+  sodiumValue: string = '0.01';
+  potassiumValue: string = '0.2';
+  phosphorusValue: string = '1';
   constructor(private router: Router) { }
 
   ngOnInit(): void {
-    // Initialization logic can go here
+    //this.loadUserInfo();
   }
 
   // Navigation methods
